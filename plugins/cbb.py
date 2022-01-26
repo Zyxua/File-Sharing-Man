424b62be4f2b73bfd81bec296a2236f889ded198
rzkyfadilla._ <81405011+poocong@users.noreply.github.com>
2022-01-26 08:52:30 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -13,7 +13,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text=f"<b>Tentang Bot ini:\n\n • Owner: @{OWNER}\n • Channel: @{CHANNEL}\n • Group: @{GROUP}\n • Source Code: <a href='https://t.me/PoocongOnlen/133'>Klik Disini</a></b>\n",
+            text=f"<b>Tentang Bot ini:\n\n • Owner: @{OWNER}\n • Channel 1: @{CHANNEL}\n • Channel 2: @{GROUP}\n • Source Code: <a href='https://t.me/PoocongOnlen/133'>Klik Disini</a></b>\n",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("• ᴛᴜᴛᴜᴘ •", callback_data="close")]]
